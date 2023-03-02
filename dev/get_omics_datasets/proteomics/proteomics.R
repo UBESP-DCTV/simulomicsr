d2de7e6a9c07ed2f2a2df145dc5daf1421869c8e
Luca Vedovelli <lucavd@gmail.com>
2023-03-02 09:58:39 +0100

    first try proteomics data

--- dev/get_omics_datasets/proteomics/proteomics.R
+++ dev/get_omics_datasets/proteomics/proteomics.R
@@ -6,29 +6,25 @@ px <- PXDataset(id)
 
 txt <- pxfiles(px) |> as.data.frame()
 
+# FILTRARE PER SPECIES!
 
-library(purrr)
+library(tidyverse)
 
-get_pxfiles_dataframe <- function() {
-  # define the range of IDs to process
-  id_range <- sprintf("PXD%06d", 1:20)
+# define the range of IDs to process
+id_range <- sprintf("PXD%06d", 1:20)
 
-  # use purrr::map with possibly to skip any IDs that result in an error
-  results <- map(
+# use purrr::map with possibly to skip any IDs that result in an error
+
+results <- map(
     id_range,
-    possibly(~ pxfiles(PXDataset(.)), otherwise = NA_character_)
-  )
+    possibly(~ pxfiles(PXDataset(.)), otherwise = NA_character_))
 
-  # convert the results to a dataframe with columns named after the IDs
-  # df <- as.data.frame(results)
-  # colnames(df) <- id_range
+names(results) <- id_range
 
-  return(results)
-}
+has_protein_groups <- map_lgl(results,
+                              ~ any(str_detect(.x, "proteinGroups.txt")))
 
-
-
-
+results_with_proteingroups <- results[has_protein_groups]
 
 
 
