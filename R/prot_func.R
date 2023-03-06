c0fd21b2bfd0b890a33dd0e55d72c04084cbb860
Corrado Lanera <Corrado.Lanera@gmail.com>
2023-03-06 15:33:32 +0100

    get proteingroups files and tibbles

--- R/prot_func.R
+++ R/prot_func.R
@@ -1,8 +1,31 @@
 pull_pxd_listfile <- function(pdxs) {
   pdxs |>
     purrr::map(purrr::possibly(
-      ~ rpx::pxfiles(rpx::PXDataset(.x)),
+      ~ rpx::pxfiles(rpx::PXDataset2(.x), n = 0L),
       otherwise = NA_character_
     )) |>
     purrr::set_names(pdxs)
+}
+
+
+have_proteingroups <- function(pxd_list) {
+  are_to_retain <- pxd_list |>
+    purrr::map_lgl(~any(stringr::str_detect(.x, "proteinGroups\\.txt")))
+}
+
+extract_with_proteins <- function(pxd_list) {
+  names(pxd_list[have_proteingroups(pxd_list)])
+}
+
+get_proteingroups_filepath <- function(pxd_list) {
+  pxd_list |>
+    purrr::map(rpx::PXDataset2) |>
+    purrr::map_chr(~{
+      rpx::pxget(.x, "proteinGroups.txt") |>
+        normalizePath()
+    })
+}
+
+read_proteingroups <- function(filepath) {
+  readr::read_tsv(filepath, show_col_types = FALSE)
 }
--- tests/testthat/test-prot_func.R
+++ tests/testthat/test-prot_func.R
@@ -4,7 +4,8 @@ test_that("pull_pxd_listfile works", {
   missing_pdxs <- compose_pxds(2)
 
   # eval
-  res <- pull_pxd_listfile(two_pdxs)
+  res <- pull_pxd_listfile(two_pdxs) |>
+    suppressMessages()
 
   # test
   expect_length(res, 2L)
@@ -14,3 +15,51 @@ test_that("pull_pxd_listfile works", {
     regexp = "400 Bad Request"
   )
 })
+
+test_that("extract_with_proteins works", {
+  # setup
+  with_prot <- compose_pxds(22)
+  no_prot <- compose_pxds(c(1, 4))
+  some_pdxs <- c(with_prot, no_prot)
+
+  # eval
+  res <- pull_pxd_listfile(some_pdxs) |>
+    extract_with_proteins() |>
+    suppressMessages()
+
+  # test
+  expect_character(res, len = 1L)
+  expect_equal(res, with_prot)
+})
+
+test_that("get_proteingroups_filepath works", {
+  # setup
+  prot_22 <- compose_pxds(c(1, 4, 22, 44))
+
+  # eval
+  res <- pull_pxd_listfile(prot_22) |>
+    extract_with_proteins() |>
+    get_proteingroups_filepath() |>
+    suppressMessages()
+
+  # test
+  expect_character(res, pattern = "proteinGroups\\.txt$", len = 2)
+  purrr::walk(res, expect_file_exists)
+})
+
+test_that("get_proteingroups_filepath works", {
+  # setup
+  prot_22 <- compose_pxds(22)
+
+  # eval
+  res <- pull_pxd_listfile(prot_22) |>
+    extract_with_proteins() |>
+    get_proteingroups_filepath() |>
+    read_proteingroups()
+
+  # test
+  expect_tibble(res)
+})
+
+
+
